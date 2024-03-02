5026390ca6d20985f58c0d4f264b4f177c65cbd3
ansarulSohan <ansarulsohan@gmail.com>
2024-03-02 08:06:14 +0100

    implemented controller for creating user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,35 @@
-const User = require("../models/users");
+const User = require("../models/User.model");
+const hashPassword = require("../utils/hashPass.util");
 
-const getUser = (req, res) => {
-  const { user } = req.user;
-  const userProfile = User.find({ user });
+const getUser = async (req, res) => {
+  const { id } = req.user;
+  const userProfile = await User.findById(id);
   if (!userProfile) {
+    return res.status(404).send({ msg: "User not found!" });
   }
 };
 
-const createUser = (req, res) => {};
+/**
+ * @function createUser
+ * @description Controller to create new users, returns new user on success, return error msg on error
+ * @param {*} req
+ * @param {*} res
+ * @returns user
+ * @returns {msg: "User already exists"} on error
+ */
+
+const createUser = async (req, res) => {
+  const { username, email, phone, password } = req.body;
+  let user = await User.findOne({ email });
+  if (user) return res.status(400).send({ msg: "User already exists" });
+  user = new User({
+    username,
+    email,
+    phone,
+    password: await hashPassword(password),
+  });
+  user = await user.save();
+  res.send(user);
+};
+
+module.exports = { getUser, createUser };
