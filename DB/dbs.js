e6fc97880423c01d4e1915763106749dda2d1f06
ansarulSohan <ansarulsohan@gmail.com>
2024-03-02 08:05:39 +0100

    Updated DB fucntion with latest mongoose version

--- DB/dbs.js
+++ DB/dbs.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
+const connectDB = async () => {
   const dbURI =
     process.env.DB_URI || "mongodb://localhost:27017/personal_expense_tracker";
-  mongoose.connect(uri, (error) => {
-    console.log("Error while connecting to DB");
-  });
+
+  try {
+    await mongoose.connect(dbURI);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 
   mongoose.connection.on("error", (err) => {
     console.log("An error occured in the database.", err);
+  });
+
+  mongoose.connection.on("disconnected", (err) => {
+    console.log("Database disconnected");
+    process.exit(1);
   });
 };
 
