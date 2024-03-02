298060a130f9ac82a2e94b650173329ef33fcc62
ansarulSohan <ansarulsohan@gmail.com>
2024-03-02 08:04:23 +0100

    Added Get and Post for users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,3 +1,7 @@
+const { getUser, createUser } = require("../controllers/users.controller");
+
 const router = require("express").Router();
 
-router.get("/");
+router.get("/", getUser).post("/", createUser);
+
+module.exports = router;
